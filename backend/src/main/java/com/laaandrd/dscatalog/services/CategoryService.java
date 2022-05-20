3d142a7c1934ed9cc56ca51f9ff7b4a85f239fc7
Lucas Andrada <laaandrd@protonmail.com>
2022-05-20 11:48:58 -0300

    H2 database

---
+++ backend/src/main/java/com/laaandrd/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.laaandrd.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.laaandrd.dscatalog.entities.Category;
+import com.laaandrd.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+}
